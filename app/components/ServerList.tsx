97cf2b066e2c7cb5557ec8e494837e7f512c6dc7
Tobias Lebeitsuk <tobias.lebeitsuk@gmail.com>
2026-03-19 14:19:45 +0200

    user menu, logout

--- app/components/ServerList.tsx
+++ app/components/ServerList.tsx
@@ -5,11 +5,9 @@ export default function ServerList() {
   return (
     <div className="w-16 flex flex-col gap-4 justify-between items-center bg-gray-800 p-4 h-full">
       <div className="space-y-4">
-
         <Avatar name="Lorem ipsum" />
         <Avatar name="Ipsumlorem awd aw" />
         <Avatar name="jee jee" />
-
       </div>
       <UserMenu />
     </div>
--- app/components/UserMenu.tsx
+++ app/components/UserMenu.tsx
@@ -1,18 +1,40 @@
+import { useState, useEffect } from "react";
 import Avatar from "./Avatar";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
 
 export default function UserMenu() {
+  const router = useRouter();
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    async function getUser() {
+      const { data } = await supabase.auth.getUser()
+
+      if (data.user) {
+        setName(data.user.user_metadata.name || data.user.email)
+      }
+    }
+
+    getUser();
+  }, []);
+
+  async function handleLogout() {
+    await supabase.auth.signOut();
+    router.push("/login");
+  }
+
   return (
     <div>
       <button popoverTarget="user-menu" className="[anchor-name:--user-menu]">
-        <Avatar name="Tobias Lebeitsuk" />
+        <Avatar name={name} />
       </button>
       <div popover="auto" id="user-menu"
         className="[position-anchor:--user-menu] left-[anchor(100%)] top-[anchor(bottom)] -translate-y-full translate-x-2 rounded-md p-2 w-42"
       >
         <ul className="space-y-1">
-          <li>Log out</li>
-          <li>Log out</li>
-          <li>Log out</li>
+          <li className="text-gray-600">{name}</li>
+          <li onClick={handleLogout} className="hover:">Log out</li>
         </ul>
       </div>
     </div>
