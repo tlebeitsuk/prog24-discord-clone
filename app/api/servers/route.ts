a3c589ab48165b9019e73c700b866e047cd2c750
Tobias Lebeitsuk <tobias.lebeitsuk@gmail.com>
2026-04-10 13:30:46 +0300

    get server list from db

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -22,3 +22,22 @@ export async function POST(req: Request) {
 
   return NextResponse.json(data)
 }
+
+export async function GET(req: Request) {
+  const supabase = await supabaseServer()
+
+  const { data: { user } } = await supabase.auth.getUser()
+
+  // Check if user exists
+  if (!user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+  }
+
+  const { data, error } = await supabase.from("servers").select('*')
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+
+  return NextResponse.json(data)
+}
--- app/components/ServerList.tsx
+++ app/components/ServerList.tsx
@@ -1,14 +1,36 @@
 import Avatar from "./Avatar";
 import UserMenu from "./UserMenu";
 import NewServer from "./NewServer";
+import { useEffect, useState } from "react";
 
 export default function ServerList() {
+  const [servers, setServers] = useState([]);
+
+  useEffect(() => {
+    async function getSession() {
+      const response = await fetch("/api/servers", {
+        method: "GET",
+      })
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.error(data.error);
+        return;
+      }
+
+      setServers(data);
+    }
+
+    getSession();
+  }, []);
+
   return (
     <div className="w-16 flex flex-col gap-4 justify-between items-center bg-gray-800 p-4 h-full">
       <div className="space-y-4">
-        <Avatar name="Lorem ipsum" />
-        <Avatar name="Ipsumlorem awd aw" />
-        <Avatar name="jee jee" />
+        {servers.map((server) => (
+          <Avatar key={server.id} name={server.name} />
+        ))}
         <NewServer />
       </div>
       <UserMenu />
